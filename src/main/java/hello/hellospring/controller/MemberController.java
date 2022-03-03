722230d778d602294a2eef77e0bc1f83ef9916ef
Jihye Kim <sos13313@naver.com>
2022-03-03 18:47:36 +0900

    회원조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,8 +5,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -20,6 +23,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    // get은 조회할 때, 데이터를 등록할 땐 post
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
@@ -27,11 +31,19 @@ public class MemberController {
 
     @PostMapping("/members/new")
     public String create(MemberForm form) {
-        Member member= new Member();
+        Member member = new Member();
         member.setName(form.getName());
 
         memberService.join(member);
 
         return "redirect:/"; //home으로 보내기
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); //members안에 모든 member를 담아놨음
+        return "members/memberList";
+    }
+
 }
--- src/main/java/hello/hellospring/controller/MemberForm.java
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -5,9 +5,9 @@ public class MemberForm {
 
     public String getName() {
         return name;
-    }
+    } // name을 꺼내서 쓴다.
 
     public void setName(String name) {
         this.name = name;
-    }
+    } // form에서 들어온 name이 여기에 들어간다.
 }
